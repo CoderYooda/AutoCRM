6dbb0c80f8e0c160848670b122f53bcb169a8305
CoderYooda <coderyooda@gmail.com>
2019-09-18 17:57:43 +0300

    Быстрая правка

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-HOME
+HOME2
 @endsection
--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -14,7 +14,7 @@
                 <div class="navbar flex-nowrap white lt box-shadow" style="z-index: 1010;">
                     <div class="flex">
                         <div class="input-group">
-                            <input id="search" type="text" value="{{ request('search') }}" class="form-control form-control-sm search"  placeholder="Поиск по складу">
+                            <input id="search" name="search" type="text" value="{{ request('search') }}" class="form-control form-control-sm search"  placeholder="Поиск по складу">
                             <span class="input-group-append">
                                 <button class="btn btn-default btn-sm no-shadow" type="button"><i class="fa fa-search"></i></button>
                             </span>
@@ -31,7 +31,6 @@
             </div>
         </div>
     </div>
-
 </div>
 
 
