16ab38643c7d0c8d7414779598e3eb851a8d6a7d
CoderYooda <coderyooda@gmail.com>
2020-03-20 17:38:29 +0300

    awd

--- app/Http/Controllers/SystemMessageController.php
+++ app/Http/Controllers/SystemMessageController.php
@@ -72,15 +72,33 @@ class SystemMessageController extends Controller
 //        ]);
     }
 
-    public static function sendToCompany($company_id, $type, $message, $model)
+    public static function sendToCompany($company_id, $type, $message, $model, $pause = null)
     {
         $company = Company::where('id', $company_id)->first();
         foreach($company->members()->get() as $user){
 
-            $sm = SM::where('reciever_id', $user->id)->where('kind', class_basename($model))->where('kind_id', $model->id)->orderBy('id', 'DESC')->first();
+            if($pause !== null){
 
-            if($sm == null || !Hash::check( class_basename($model) . $model->id . $model->updated_at, $sm->hash)){
-                echo 'Создаём сообщение';
+                $sm = SM::where('reciever_id', $user->id)->where('kind', class_basename($model))->where('kind_id', $model->id)->orderBy('id', 'DESC')->first();
+
+                if($sm == null || !Hash::check( class_basename($model) . $model->id . $model->updated_at, $sm->hash)){ //
+                    echo 'Создаём сообщение';
+                    $system_message = new SM();
+                    $system_message->user_id = 1;
+                    $system_message->reciever_id = $user->id;
+                    $system_message->kind_id = $model->id;
+                    $system_message->kind = class_basename($model);
+                    $system_message->type = $type;
+                    $system_message->hash = Hash::make( class_basename($model) . $model->id . $model->updated_at );
+                    $system_message->message = $message;
+                    $system_message->save();
+
+                    event(
+                        new SystemMessage($system_message)
+                    );
+                }
+
+            } else {
                 $system_message = new SM();
                 $system_message->user_id = 1;
                 $system_message->reciever_id = $user->id;
@@ -95,6 +113,7 @@ class SystemMessageController extends Controller
                     new SystemMessage($system_message)
                 );
             }
+
         }
     }
 }
