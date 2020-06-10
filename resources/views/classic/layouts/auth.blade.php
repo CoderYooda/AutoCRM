46d718ea8e483ceb0725810c0d8794bd860cb7b6
Mikhail <mihail@webstyle.top>
2020-06-10 10:01:43 +0300

    add YandexMetrika

--- resources/views/classic/layouts/auth.blade.php
+++ resources/views/classic/layouts/auth.blade.php
@@ -13,7 +13,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/base.css') }}" rel="stylesheet">
-{{--    <link href="{{ asset('dependencies/particles/style.css') }}" rel="stylesheet">--}}
 </head>
     <body>
 
@@ -53,5 +52,21 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="{{ asset('dependencies/particles/particles.js') }}" defer></script>
         <script src="{{ asset('dependencies/particles/app.js') }}" defer></script>
+
+        <!— Yandex.Metrika counter —>
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(64758580, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/64758580" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!— /Yandex.Metrika counter —>
     </body>
 </html>
