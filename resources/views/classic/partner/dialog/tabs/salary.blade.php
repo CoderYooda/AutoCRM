52c032811446a7385fc982aeef0427f8e2ee6de1
CoderYooda <coderyooda@gmail.com>
2020-09-21 10:43:43 +0300

    fix

--- resources/views/classic/partner/dialog/tabs/salary.blade.php
+++ resources/views/classic/partner/dialog/tabs/salary.blade.php
@@ -1,20 +1,16 @@
-
 <div class="form-group fl ip ul">
-
-
-
-
         <label>Зарплатные настройки</label>
         <div>
 
         </div>
         <div data-simplebar style="max-height: 400px">
             <div class="schema_stored">
-                @foreach($partner->salarySchemas as $index => $schema)
-                    @include(get_template() . '.partner.dialog.tabs.includes.salary_schema')
-                @endforeach
+                @if($partner)
+                    @foreach($partner->salarySchemas as $index => $schema)
+                        @include(get_template() . '.partner.dialog.tabs.includes.salary_schema')
+                    @endforeach
+                @endif
             </div>
         </div>
         <button type="button" class="button mt-10" onclick="window.{{ $class }}.openSalarySchemaModal()">Добавить настройку</button>
-
 </div>
