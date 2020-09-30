3a459fed8c99129e5c7362780b52b28147201f4b
CoderYooda <CoderYooda@gmail.com>
2020-09-30 22:58:24 +0300

    Verstka

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,12 +4,24 @@
 <div class="header">
     <div class="header-top">
         <div class="container">
-            <div class="mini-card">г. Белгород, Проспект Славы 150А</div>
-
+            <div class="mini-card pin">
+                <span>г. Белгород, Проспект Славы 150А</span>
+            </div>
+            <div class="mini-card float-r phone">
+                <span>8(952)436-50-64</span>
+            </div>
+            <div class="mini-card float-r pr-50 mail">
+                <span>CoderYooda@gmail.com</span>
+            </div>
         </div>
     </div>
     <div class="header-mid">
-
+        <div class="container">
+            <div class="logo-container">
+                <div class="logo"></div>
+                <div class="company-name">Название вашей компании</div>
+            </div>
+        </div>
     </div>
     <div class="header-bottom">
 
