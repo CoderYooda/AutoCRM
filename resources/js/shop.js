cbcdb70139f0d39ff3ceebfab0a00513315fec33
CoderYooda <CoderYooda@gmail.com>
2020-10-04 17:18:36 +0300

    awd

--- resources/js/shop.js
+++ resources/js/shop.js
@@ -1,1 +1,76 @@
-console.log(123);
+import BBSlider from '../modules/bbslider/bbslider.min';
+
+window.headSlider = new BBSlider({
+    selector: '.head-slider-container',
+    duration: 600,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+    onInit: () => {
+    },
+    onChange: () => {activatePin()},
+});
+
+let pinsContainer = document.querySelector('.pins-container');
+if(pinsContainer){
+    let slideIndex = 0;
+    window.headSlider.innerElements.forEach((elem) => {
+        let pin = document.createElement('div');
+        pin.classList.add('pin');
+        pin.onclick = function(){
+            window.headSlider.goTo(this.getAttribute('data-slide-id'));
+            activatePin();
+        };
+        pin.setAttribute('data-slide-id', slideIndex);
+        pinsContainer.appendChild(pin);
+        slideIndex++;
+    });
+    activatePin();
+}
+
+
+function activatePin(){
+    let activePin = document.querySelector('[data-slide-id="' + window.headSlider.currentSlide + '"]');
+    let pins = document.querySelectorAll('.pin');
+    pins.forEach((elem) => {
+        elem.classList.remove('active');
+    });
+    if(activePin){
+        activePin.classList.add('active');
+    }
+}
+
+
+let pop_products = document.querySelector('.popular-products');
+if(pop_products){
+    if(pop_products.querySelectorAll('.product').length > 4){
+        window.popularSlider = new BBSlider({
+            selector: '.popular-products',
+            duration: 600,
+            easing: 'ease-out',
+            perPage: 4,
+            startIndex: 0,
+            draggable: true,
+            multipleDrag: true,
+            threshold: 20,
+            loop: true,
+            rtl: false,
+            onInit: () => {
+            },
+            onChange: () => {},
+        });
+    } else {
+        let controls = pop_products.parentElement.querySelector('.controls');
+        controls.style.display = 'none';
+    }
+}
+
+
+
+
+
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -56,17 +56,22 @@
 <div class="body">
     <div class="slider container">
         <div class="silder-content">
-            <img style="display: block;" src="/images/shop/slider.png" alt=""> {{-- Это дерьмо меняем на слайдер --}}
+            <div class="head-slider-container">
+                <img style="display: block;" src="/images/shop/slider.png" alt="">
+                <img style="display: block;" src="/images/shop/slider.png" alt="">
+                <img style="display: block;" src="/images/shop/slider.png" alt="">
+                <img style="display: block;" src="/images/shop/slider.png" alt="">
+            </div>
             <div class="controls">
-                <div class="control-item left"></div>
-                <div class="control-item right"></div>
+                <div class="control-item left" onclick="window.headSlider.prev()"></div>
+                <div class="control-item right" onclick="window.headSlider.next()"></div>
             </div>
             <div class="pins-container">
-                <div class="pin"></div>
-                <div class="pin"></div>
-                <div class="pin active"></div>
-                <div class="pin"></div>
-                <div class="pin"></div>
+{{--                <div class="pin"></div>--}}
+{{--                <div class="pin"></div>--}}
+{{--                <div class="pin active"></div>--}}
+{{--                <div class="pin"></div>--}}
+{{--                <div class="pin"></div>--}}
             </div>
         </div>
     </div>
@@ -74,46 +79,48 @@
         <div class="title">
             <h2>Популярные товары</h2>
             <div class="products grid-4">
-                @for($i = 0; $i <= 3; $i++)
-                <div class="product">
-                    <img class="product-img" title="сюда имя товара" src="/images/shop/product-{{ rand(0,3) }}.png" alt="">
-                    <h3 class="product-name" title="Название товара!!!">Моторное масло</h3>
-                    <div class="brand">VAG</div>
-                    <div class="article">k1279</div>
-                    <div class="price-container">
-                        @php $isAction = rand(0,1); $price = rand(1000, 300000) @endphp
-                        <span class="price @if($isAction) action @endif">{{ number_format($price, 0, ',', ' ') }}</span>
-                        @if($isAction)
-                        <span class="strikethrough-price">{{ number_format($price + rand(1000, 900000), 0, ',', ' ') }}</span>
-                        @endif
-                    </div>
-                    <div class="top-left-label">
-                        @if(rand(0,1))
-                            <div class="in-stock">В наличии</div>
-                        @else
-                            <div class="out-of-stock">Под заказ</div>
-                        @endif
-                        @if($isAction)
-                        <div class="discount">-30%</div>
-                        @endif
-                    </div>
-                    <div class="top-right-label">
-                        <div class="favour"></div>
-                        <div class="info"></div>
-                    </div>
-                    <div class="shipping-container">
-                        <div class="counter-container">
-                            <div class="button minus"></div>
-                            <input class="counter" value="1" type="text">
-                            <div class="button plus"></div>
+                <div class="popular-products">
+                    @for($i = 0; $i < 5; $i++)
+                    <div class="product">
+                        <img class="product-img" title="сюда имя товара" src="/images/shop/product-{{ rand(0,3) }}.png" alt="">
+                        <h3 class="product-name" title="Название товара!!!">Моторное масло</h3>
+                        <div class="brand">VAG</div>
+                        <div class="article">k1279</div>
+                        <div class="price-container">
+                            @php $isAction = rand(0,1); $price = rand(1000, 300000) @endphp
+                            <span class="price @if($isAction) action @endif">{{ number_format($price, 0, ',', ' ') }}</span>
+                            @if($isAction)
+                            <span class="strikethrough-price">{{ number_format($price + rand(1000, 900000), 0, ',', ' ') }}</span>
+                            @endif
                         </div>
-                        <div class="cart-button {{ rand(0,1) ? 'incart' : '' }}"></div>
+                        <div class="top-left-label">
+                            @if(rand(0,1))
+                                <div class="in-stock">В наличии</div>
+                            @else
+                                <div class="out-of-stock">Под заказ</div>
+                            @endif
+                            @if($isAction)
+                            <div class="discount">-30%</div>
+                            @endif
+                        </div>
+                        <div class="top-right-label">
+                            <div class="favour"></div>
+                            <div class="info"></div>
+                        </div>
+                        <div class="shipping-container">
+                            <div class="counter-container">
+                                <div class="button minus"></div>
+                                <input class="counter" value="1" type="text">
+                                <div class="button plus"></div>
+                            </div>
+                            <div class="cart-button {{ rand(0,1) ? 'incart' : '' }}"></div>
+                        </div>
                     </div>
+                    @endfor
                 </div>
-                @endfor
                 <div class="controls">
-                    <div class="control-item left"></div>
-                    <div class="control-item right"></div>
+                    <div class="control-item left" onclick="window.popularSlider.prev()"></div>
+                    <div class="control-item right" onclick="window.popularSlider.next()"></div>
                 </div>
             </div>
         </div>
