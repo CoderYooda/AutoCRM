3f9c5e2c8732d1c6cc80f0e7de144b6200e14c66
unknown <andrey.malishev77@gmail.com>
2020-11-20 17:30:09 +0300

    123

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,6 @@ use App\Mail\Shop\PayedOrder;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-
-    $pdf = App::make('dompdf.wrapper');
-    $pdf->loadHTML('<h1>Test</h1>');
-    return $pdf->stream();  
-
-});
-
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('PostLogin');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
