5ef6bab1591c60ec446e8e03dea77ef8b11557bc
vladvasilenko <VladVasilenko20132014@gmailc.om>
2021-02-05 14:28:56 +0300

    Правки на редактирование товаров

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -41,7 +41,7 @@ class ProductRequest extends FormRequest
 
             'image' => ['file', 'mimes:jpg,jpeg,png,gif', 'max:5120'],
 
-            'barcode' => ['nullable', Rule::unique('products', 'barcode')->where('company_id', Auth::user()->company_id)->ignore($this->id)],
+            'barcode' => ['nullable', 'digits_between:13,13', Rule::unique('products', 'barcode')->where('company_id', Auth::user()->company_id)->ignore($this->id)],
 
             'shop.name' => ['nullable', 'string', 'max:255'],
             'shop.desc' => ['nullable', 'string', 'max:1024'],
